b2e8c6f2d7010a4c540e179280d43fe0b87183c4
Irfan Zidni <irfan.zidni@qiscus.net>
2024-10-31 14:04:35 +0700

    add model and migration card

---
+++ app/Enums/CardPriority.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+enum CardPriority: string {
+    case URGENT = 'Urgent';
+    case HIGHT = 'Hight';
+    case MEDIUM = 'Medium';
+    case LOW = 'Low';
+    case UNKNOWN = 'Unknown';
+
+    public static function options(): array
+    {
+        return collect(self::cases())->map(fn($item) => [
+            'value' => $item->value,
+            'label' => $item->name,
+        ])->values()->toArray();
+    }
+}
