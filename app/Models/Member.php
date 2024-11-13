b23a217e175e58b17b6eab720bd65aba81fa4297
Irfan Zidni <irfanzidniofficial@gmail.com>
2024-11-13 09:40:33 +0700

    add relations model

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Member extends Model
 {
     use HasFactory;
-    protected $guarded=[];
+    protected $guarded = [];
+
+    protected $with = ['user'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function memberable(): MorphTo
+    {
+        return $this->morphTo();
+    }
 }
