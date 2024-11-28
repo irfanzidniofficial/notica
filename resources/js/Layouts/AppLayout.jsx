e75e478e2afa7d84e5548423ac386f300f219786
Irfan Zidni <irfanzidniofficial@gmail.com>
2024-11-28 14:19:16 +0700

    add sidebar responsive

--- resources/js/Layouts/AppLayout.jsx
+++ resources/js/Layouts/AppLayout.jsx
@@ -57,6 +57,84 @@ export default function AppLayout({ children, title }) {
                                         </div>
                                     </Transition.Child>
                                     {/* Sidebar Responsive */}
+                                    <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2 dark:bg-gray-900">
+                                        <div className="flex h-16 shrink-0 items-center space-x-1.5">
+                                            <Link
+                                                href="/"
+                                                className="-m-1.5 p-1.5 text-2xl font-black leading-relaxed tracking-tighter"
+                                            >
+                                                Plannify<span className="text-red-500">.</span>
+                                            </Link>
+                                        </div>
+                                        <nav className="flex flex-1 flex-col">
+                                            <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                                                <li>
+                                                    <ul role="list" className="-mx-2 space-y-1">
+                                                        {/* menu */}
+                                                        <li>
+                                                            <Link
+                                                                href="#"
+                                                                className="flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
+                                                            >
+                                                                <PiHouse className="h-6 w-6 shrink-0 text-foreground" />
+                                                                Dashboard
+                                                            </Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link
+                                                                href="#"
+                                                                className="flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
+                                                            >
+                                                                <PiUser className="h-6 w-6 shrink-0 text-foreground" />
+                                                                People
+                                                            </Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link
+                                                                href="#"
+                                                                className="flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
+                                                            >
+                                                                <PiSquaresFour className="h-6 w-6 shrink-0 text-foreground" />
+                                                                My Tasks
+                                                            </Link>
+                                                        </li>
+                                                        <li>
+                                                            <Link
+                                                                href="#"
+                                                                className="flex gap-x-3 rounded-md p-3 text-sm font-semibold leading-relaxed text-foreground hover:bg-gray-100"
+                                                            >
+                                                                <PiLockKeyOpen className="h-6 w-6 shrink-0 text-foreground" />
+                                                                Logout
+                                                            </Link>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li>
+                                                    {/* workspaces */}
+                                                    <div>
+                                                        <div className="flex items-center justify-between">
+                                                            <div className='text-xs font-semibold leading-relaxed uppercase  text-foreground hover:text-red-500'>Workspace</div>
+                                                            <Link>
+                                                                <PiPlus className="h-4 w-4 text-foreground" />
+                                                            </Link>
+                                                        </div>
+                                                    </div>
+                                                    <ul role='list' className='mt-2 -mx-2 space-y-1'>
+                                                        <li>
+
+                                                            <Link href='#' className='flex w-full p-3 text-sm font-semibold leading-relaxed rounded-md group gap-x-3 text-foreground hover:bg-gray-100'>
+                                                            <span className='flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border bg-white text-[0.625rem] font-medium border-foreground text-foreground'>
+                                                                B
+                                                            </span>
+                                                            <span className='truncate'>Backend Developer </span>
+                                                            </Link>
+
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
                                 </Dialog.Panel>
                             </Transition.Child>
                         </div>
